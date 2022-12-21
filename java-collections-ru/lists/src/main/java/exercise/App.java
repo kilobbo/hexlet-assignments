a059336b482144ff1e41301123d0c494468a9ef4
@hexlet/cli <support@hexlet.io>
2022-12-21 16:07:11 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -10,11 +10,11 @@ class App {
         word = word.toLowerCase();
         List<String> symbolsList = new ArrayList<>();
 
-        for (var i = 1; i < symbols.length(); i++) {
+        for (var i = 1; i <= symbols.length(); i++) {
             symbolsList.add(symbols.substring(i - 1, i));
         }
 
-        for (var i = 1; i < word.length(); i++) {
+        for (var i = 1; i <= word.length(); i++) {
             if (!symbolsList.contains(word.substring(i - 1, i))) {
                 return false;
             }
