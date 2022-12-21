562228ab692928f72346f6338d46761815af4240
@hexlet/cli <support@hexlet.io>
2022-12-21 15:18:48 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -8,14 +8,14 @@ import java.util.ArrayList;
 class App {
     public static boolean scrabble(String symbols, String word) {
         word = word.toLowerCase();
-        List<String> symbolsList = new ArrayList<>();
-
-        for (var i = 1; i < symbols.length(); i++) {
-            symbolsList.add(symbols.substring(i - 1, i));
-        }
+        List<String> wordList = new ArrayList<>();
 
         for (var i = 1; i < word.length(); i++) {
-            if (symbolsList.indexOf(word.substring(i - 1, i)) == -1) {
+            wordList.add(word.substring(i - 1, i));
+        }
+
+        for (var i = 1; i < symbols.length(); i++) {
+            if (wordList.indexOf(symbols.substring(i - 1, i)) == -1) {
                 return false;
             }
         }
