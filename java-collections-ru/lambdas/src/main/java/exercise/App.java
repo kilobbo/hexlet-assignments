d1d345f7c0dba348166c2ea42198d7affd6893bb
@hexlet/cli <support@hexlet.io>
2022-12-29 21:28:16 +0300

    submit java-collections-ru/lambdas

--- java-collections-ru/lambdas/src/main/java/exercise/App.java
+++ java-collections-ru/lambdas/src/main/java/exercise/App.java
@@ -1,13 +1,14 @@
 package exercise;
 
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 // BEGIN
 class App {
     public static String[][] enlargeArrayImage(String[][] arrayImage) {
         String[][] doubledRows = new String[arrayImage.length * 2][arrayImage.length * 2];
 
-        String[][] res = Arrays.stream(doubledRows)
+        String[][] res = Arrays.stream(arrayImage)
                 .flatMap(row -> Stream.of(row, row))
                 .toArray(String[][]::new);
 
